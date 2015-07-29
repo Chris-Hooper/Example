29d96c90da74bfa3c82e28fe73e22ed69a0f5320
Chris Hooper <ThePerfectElement89@gmail.com>
2015-07-28 21:45:21 -0400

    Created test java project

---
+++ RandomTest/src/com/gitexample/randomtest/RandomTest.java
@@ -0,0 +1,15 @@
+package com.gitexample.randomtest;
+
+public class RandomTest {
+
+	
+	public static void main(String[] args) {
+		String name = "Leonardo";
+		
+		System.out.println("Hello there " + name);
+		
+		
+		
+	}
+
+}
